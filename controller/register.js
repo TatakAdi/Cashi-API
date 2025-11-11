d7c484a15a52d552697d87f0ab50b0b88265bf91
TatakAdi <tatakadisubagyo@gmail.com>
2025-11-12 02:29:16 +0800

    feat: menambahkan default categories pas pertama kali daftar aplikasi

--- controller/register.js
+++ controller/register.js
@@ -2,6 +2,26 @@ const prisma = require("../config/prisma");
 const supabase = require("../config/supabase");
 const bcrypt = require("bcrypt");
 
+const defaultCategories = [
+  // Expenses
+  { name: "Makanan & Minuman", type: "expense" },
+  { name: "Transportasi", type: "expense" },
+  { name: "Belanja", type: "expense" },
+  { name: "Hiburan", type: "expense" },
+  { name: "Tagihan", type: "expense" },
+  { name: "Pendidikan", type: "expense" },
+  { name: "Kesehatan", type: "expense" },
+  { name: "Hadiah / Donasi", type: "expense" },
+  { name: "Lainnya", type: "expense" },
+
+  // Income
+  { name: "Gaji", type: "income" },
+  { name: "Bonus", type: "income" },
+  { name: "Investasi", type: "income" },
+  { name: "Hadiah", type: "income" },
+  { name: "Lainnya", type: "income" },
+];
+
 exports.register = async (req, res) => {
   const { email, password, fullname } = req.body;
 
@@ -30,12 +50,17 @@ exports.register = async (req, res) => {
       },
     });
 
-    res
-      .status(201)
-      .json({
-        message:
-          "User berhasil melakukan registrasi, silahkan konfirmasi email terlebih dahulu sebelum melanjutkan",
-      });
+    await prisma.category.createMany({
+      data: defaultCategories.map((cat) => ({
+        ...cat,
+        user_id: data.user.id,
+      })),
+    });
+
+    res.status(201).json({
+      message:
+        "User berhasil melakukan registrasi, silahkan konfirmasi email terlebih dahulu sebelum melanjutkan",
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
